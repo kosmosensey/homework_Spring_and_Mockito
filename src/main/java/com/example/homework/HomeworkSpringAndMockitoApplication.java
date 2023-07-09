e6485fb57ed88b4749a6b31cac285aedfa91d165
kosmo <kosmosensei@gmail.com>
2023-07-09 16:58:50 +0700

    Initial commit

---
+++ src/main/java/com/example/homework/HomeworkSpringAndMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.homework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HomeworkSpringAndMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HomeworkSpringAndMockitoApplication.class, args);
+	}
+
+}
